ff5e91cdc3d64ea3224da90e9423b4d32561f699
RPrasetyoB <rpbasuki.dev@gmail.com>
2024-02-27 23:51:02 +0700

    feat: add Express typescript template

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express()
+// body parser
+app.use(express.json());
+// set cors accept all
+app.use(cors())
+// server port
+const server_port = process.env.SERVER_PORT || 3001;
+// API root route
+app.get('/', (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Welcome to DutchPay API"
+    })
+})
+//server listening
+app.listen(server_port, ()=> {
+  console.log(`server listening at http://localhost:${server_port}`)
+});
+// export app for vercel
+export default app
