f5b3d59a18e289c05111a59e41aadc997115a0ff
Sherin Olivia <sochronicle@gmail.com>
2024-03-01 22:36:47 +0700

    chore: activated user route

---
+++ src/routes/v1/users/index.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+import { getUserProfile } from '../../../controllers/user';
+
+const userRouter = express.Router()
+
+userRouter.get('/profile', getUserProfile);
+
+export default userRouter;
