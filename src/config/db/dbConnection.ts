767cc13963e56970b93b2eb44128495839ba7a51
RPrasetyoB <rpbasuki.dev@gmail.com>
2024-03-02 01:09:08 +0700

    erfactor: export dbConnect

--- src/config/db/dbConnection.ts
+++ src/config/db/dbConnection.ts
@@ -4,22 +4,22 @@ export const prisma = new PrismaClient({
     log: ["info"]
   });
   
-  const connectDB = async ()=> {
-    return await prisma.$connect();
+export const connectDB = async ()=> {
+  return await prisma.$connect();
+}
+
+export const disconnectDB = async ()=> {
+  await prisma.$disconnect();
+}
+
+
+export const db = async ()=> {
+  try {
+    await connectDB();
+    console.log("connected to database");
+  } catch (error) {
+    console.error('Error:', error);
+  } finally {
+    await disconnectDB();
   }
-  
-  const disconnectDB = async ()=> {
-    await prisma.$disconnect();
-  }
-  
-  
-  export const db = async ()=> {
-    try {
-      await connectDB();
-      console.log("connected to database");
-    } catch (error) {
-      console.error('Error:', error);
-    } finally {
-      await disconnectDB();
-    }
-  }
+}
