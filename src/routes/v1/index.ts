5b91e6618a80ca2bebc2f08e8f10278c921c6aba
Sherin Olivia <sochronicle@gmail.com>
2024-03-07 16:21:34 +0700

    chore: added friends router to routes

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
-import userRouter from './user';
-import { authenticate } from 'passport';
 import authRouter from './auth';
+import userRouter from './user';
+import friendRouter from './friend';
+import { authenticate } from 'passport';
+import authenticationMiddleware from '../../middlewares/authentication';
 
 const v1Router = express.Router()
 
 v1Router.use('/auth', authRouter)
-v1Router.use('/user', userRouter);
+v1Router.use('/user', authenticationMiddleware, userRouter);
+v1Router.use('/friend', authenticationMiddleware, friendRouter);
 
 export default v1Router
