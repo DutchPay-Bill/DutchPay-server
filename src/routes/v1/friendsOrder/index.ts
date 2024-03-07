25f92752558e8cb37da124e1206fe5724efb3547
Sherin Olivia <sochronicle@gmail.com>
2024-03-07 23:28:33 +0700

    feat: added put method for update friend order status

--- src/routes/v1/friendsOrder/index.ts
+++ src/routes/v1/friendsOrder/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createFriendsOrder } from '../../../controllers/friendsOrder';
+import { createFriendsOrder, updateFriendOrderStatus } from '../../../controllers/friendsOrder';
 
 const friendsOrderRouter = express.Router()
 
 friendsOrderRouter.post('/', createFriendsOrder)
+friendsOrderRouter.put('/', updateFriendOrderStatus)
 
 export default friendsOrderRouter;
