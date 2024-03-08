c71c6bb39b0700529fa2353a975903be40ecfb67
Sherin Olivia <sochronicle@gmail.com>
2024-03-08 21:20:34 +0700

    chore: added bill router to routes

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -3,6 +3,7 @@ import authRouter from './auth';
 import userRouter from './user';
 import friendRouter from './friend';
 import friendsOrderRouter from './friendsOrder';
+import billRouter from './bill';
 import { authenticate } from 'passport';
 import authenticationMiddleware from '../../middlewares/authentication';
 
@@ -12,5 +13,6 @@ v1Router.use('/auth', authRouter)
 v1Router.use('/user', authenticationMiddleware, userRouter);
 v1Router.use('/friend', authenticationMiddleware, friendRouter);
 v1Router.use('/friends-order', authenticationMiddleware, friendsOrderRouter);
+v1Router.use('/bill', authenticationMiddleware, billRouter);
 
 export default v1Router
