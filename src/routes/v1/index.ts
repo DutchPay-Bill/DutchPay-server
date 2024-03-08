76b98d805a1aa64294b8c35385b68b546a725b2c
Sherin Olivia <sochronicle@gmail.com>
2024-03-08 21:23:55 +0700

    chore: updated v1 router

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -3,6 +3,7 @@ import authRouter from './auth';
 import userRouter from './user';
 import friendRouter from './friend';
 import friendsOrderRouter from './friendsOrder';
+import orderRouter from './order';
 import billRouter from './bill';
 import { authenticate } from 'passport';
 import authenticationMiddleware from '../../middlewares/authentication';
@@ -12,6 +13,7 @@ const v1Router = express.Router()
 v1Router.use('/auth', authRouter)
 v1Router.use('/user', authenticationMiddleware, userRouter);
 v1Router.use('/friend', authenticationMiddleware, friendRouter);
+v1Router.use('/orders', authenticationMiddleware, orderRouter);
 v1Router.use('/friends-order', authenticationMiddleware, friendsOrderRouter);
 v1Router.use('/bill', authenticationMiddleware, billRouter);
 
