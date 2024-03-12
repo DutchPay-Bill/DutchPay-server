0ac2760d45cdc94e463b76ae137af1897914ec23
RPrasetyoB <rpbasuki.dev@gmail.com>
2024-03-12 20:04:45 +0700

    fix: cors setting

--- src/middlewares/corsOption.ts
+++ src/middlewares/corsOption.ts
@@ -2,32 +2,23 @@ import cors, { CorsOptions } from "cors";
 import { Application, Request } from "express";
 import { api_url } from "../utils/url";
 
-const origin = [
+const allowedOrigins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://dutchpay-bill.web.app",
     "http://localhost:3000",
-    `${api_url}`
+    api_url
 ];
 
-const corsOptions = (req: Request | any, callback: (err: Error | null, options?: CorsOptions) => void) => {
-    const clientOrigin = origin.includes(req.header("Origin"));
-    const isPostman = req.header("User-Agent")?.includes("Postman");
-    if (clientOrigin) {
-        callback(null, {
-            origin: true,
-            methods: 'GET, POST, DELETE, PUT, PATCH, OPTIONS, HEAD',
-            credentials: true
-        });
-    } else if (isPostman) {
-        callback(null, {
-            origin: 'https://www.getpostman.com',
-            methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD',
-            credentials: true,
-        });
-    } else {
-        callback(new Error('Not allowed by CORS'));
-    }
+const corsOptions = {
+    origin: function (origin: any, callback: any) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,
 };
 
 const corsMiddleware = (app: Application) => {
