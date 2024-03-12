cd211b17ddc7673913000f25de3c52df9d1a0960
RPrasetyoB <rpbasuki.dev@gmail.com>
2024-03-12 19:53:51 +0700

    fix: cors setting

--- src/middlewares/corsOption.ts
+++ src/middlewares/corsOption.ts
@@ -1,10 +1,13 @@
 import cors, { CorsOptions } from "cors";
 import { Application, Request } from "express";
+import { api_url } from "../utils/url";
 
 const origin = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://dutchpay-bill.web.app"
+    "https://dutchpay-bill.web.app",
+    "http://localhost:3000",
+    `${api_url}`
 ];
 
 const corsOptions = (req: Request | any, callback: (err: Error | null, options?: CorsOptions) => void) => {
@@ -20,7 +23,7 @@ const corsOptions = (req: Request | any, callback: (err: Error | null, options?:
         callback(null, {
             origin: 'https://www.getpostman.com',
             methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD',
-            credentials: true
+            credentials: true,
         });
     } else {
         callback(new Error('Not allowed by CORS'));
